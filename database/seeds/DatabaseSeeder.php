dadd173968b2be989487700c3dd8ea86aaddccec
柏倉希奈 <>
2023-03-20 23:45:56 +0900

    シーダー作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);//呼び出すように追加
+        $this->call(SubjectsTableSeeder::class);
+
 
     }
+
 }
