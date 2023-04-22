45cc10c076704b7b082f495a6ba47a88b8820383
柏倉希奈 <>
2023-04-22 12:13:32 +0900

    リレーションの追加

--- app/Models/Users/Subjects.php
+++ app/Models/Users/Subjects.php
@@ -15,7 +15,7 @@ class Subjects extends Model
         'subject'
     ];
 
-    public function user(){
-        return $this->belongsToMany('App\Models\Users\User');// リレーションの定義
+    public function users(){
+        return $this->belongsToMany('App\Models\Users\User','subject_users','subject_id','user_id');// リレーションの定義
     }
 }
