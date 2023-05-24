4962e4fad8259d4b5924b76ee61ee3bc81c6c161
柏倉希奈 <>
2023-05-24 15:24:26 +0900

    予約削除機能モーダル追加

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,3 +1,17 @@
-$(function(){
+$(function () {
+  $('.delete-modal-open').on('click', function () {
+    $('.js-modal').fadeIn();
+    var date = $(this).attr('date');
+    var part = $(this).attr('part');
+    // var reservePart = $(this).attr('reservePart');
+    $('.modal-inner-title input').val(date);
+    $('.modal-inner-body input').val(part);
+    // $('.delete-modal-hidden').val(reservePart);
+    return false;
+  });
+  $('.js-modal-close').on('click', function () {
+    $('.js-modal').fadeOut();
+    return false;
+  });
 
 });
