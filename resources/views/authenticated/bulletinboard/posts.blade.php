2e7971dfada2bae53c730fae1da4dc538fc781f2
柏倉希奈 <>
2023-05-30 00:56:59 +0900

    掲示板css追加

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -9,7 +9,7 @@
       <p><span>{{ $post->user->over_name }}</span><span class="ml-3">{{ $post->user->under_name }}</span>さん</p>
       <p><a href="{{ route('post.detail', ['id' => $post->id]) }}">{{ $post->post_title }}</a></p>
       @foreach($post->subCategories as $sub_category)
-        <span>{{ $sub_category->sub_category }}</span></li>
+        <span class="post-subCategories">{{ $sub_category->sub_category }}</span></li>
         @endforeach
       <div class="post_bottom_area d-flex">
         <div class="d-flex post_status">
@@ -29,25 +29,38 @@
     @endforeach
   </div>
   <div class="other_area border w-25">
-    <div class="border m-4">
-      <div class=""><a href="{{ route('post.input') }}">投稿</a></div>
-      <div class="">
-        <input type="text" placeholder="キーワードを検索" name="keyword" form="postSearchRequest">
-        <input type="submit" value="検索" form="postSearchRequest">
-      </div>
-      <input type="submit" name="like_posts" class="category_btn btn-danger" value="いいねした投稿" form="postSearchRequest">
-      <input type="submit" name="my_posts" class="category_btn btn-warning" value="自分の投稿" form="postSearchRequest">
-      <ul>
-        @foreach($categories as $category)
-        <li class="main_categories" category_id="{{ $category->id }}"><span>{{ $category->main_category }}</span></li>
-        @foreach($category->subCategories as $sub_category)
-        <li><input type="submit" name="category_word" class="category_btn btn-warning" value="{{ $sub_category->sub_category }}" form="postSearchRequest"></li>
-        <!-- <li class="sub_categories" category_id="{{ $sub_category->id }}"><span>{{ $sub_category->sub_category }}</span></li> -->
-        @endforeach
-        @endforeach
-      </ul>
-    </div>
+  <div class="m-4">
+  <div class="post-text"><a href="{{ route('post.input') }}" style="color:white;">投稿</a></div>
+  <div class="d-flex">
+  <input type="text" placeholder="キーワードを検索" name="keyword" class="post-search" form="postSearchRequest">
+  <input type="submit" class="post-search-button" value="検索" form="postSearchRequest">
+  </div>
+  <div class="d-flex">
+  <div class="like_posts">
+  <input type="submit" name="like_posts" class="like_posts" value="いいねした投稿" form="postSearchRequest">
+  </div>
+  <div class="my_posts">
+  <input type="submit" name="my_posts" class="my_posts" value="自分の投稿" form="postSearchRequest">
+  </div>
+  </div>
+
+  <p>カテゴリー</p>
+  <!-- アコーディオンメニュー -->
+  <div id="accordion" class="accordion-container">
+  <ul class="menu">
+  @foreach($categories as $category)
+  <li class="main_categories" category_id="{{ $category->id }}"><span>{{ $category->main_category }}</span></li>
+
+  <div class="accordion-title js-accordion-title">
+  </div>
+  @foreach($category->subCategories as $sub_category)
+  <li><input type="submit" name="category_word" class="category_btn btn-secondary" value="{{ $sub_category->sub_category }}" form="postSearchRequest"></li>
+  @endforeach
+  @endforeach
+  </ul>
+  </div>
+  </div>
   </div>
   <form action="{{ route('post.show') }}" method="get" id="postSearchRequest"></form>
-</div>
+  </div>
 @endsection
