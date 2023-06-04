0cbc4d985270c7d80cea83cbcff2b82c257ca73b
柏倉希奈 <>
2023-06-04 16:45:50 +0900

    修正

--- app/Http/Controllers/Authenticated/Calendar/Admin/CalendarsController.php
+++ app/Http/Controllers/Authenticated/Calendar/Admin/CalendarsController.php
@@ -20,8 +20,11 @@ class CalendarsController extends Controller
     }
 
     public function reserveDetail($user_id = 0, $date, $part){
+        //dd($part);
         $reservePersons = ReserveSettings::with('users')->where('setting_reserve', $date)->where('setting_part', $part)->get();
+        //dd($reservePersons);
         return view('authenticated.calendar.admin.reserve_detail', compact('reservePersons', 'date', 'part'));
+
     }
 
     public function reserveSettings(){
